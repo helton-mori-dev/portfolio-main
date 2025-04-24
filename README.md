42e597a4c062c1a3741a5d78d193a1b55cee22cf
Helton Mori <heltonmori@gmail.com>
2025-04-24 15:39:14 -0300

    About me

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Getting Started with Create React App
 
+Using node v22.13.0
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import PageHeader from "./components/PageHeader";
-// import AboutMe from "./components/AboutMe";
+import AboutMe from "./components/AboutMe";
 // import MyProjects from "./components/MyProjects";
 // import PageFooter from "./components/PageFooter";
 import "./i18n";
@@ -9,9 +9,11 @@ function App() {
   return (
     <>
       <PageHeader />
-      {/* <AboutMe />
-      <MyProjects />
-      <PageFooter /> */}
+      {
+        <AboutMe />
+        /*<MyProjects />
+      <PageFooter /> */
+      }
     </>
   );
 }
