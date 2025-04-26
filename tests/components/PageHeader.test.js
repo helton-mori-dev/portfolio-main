2882958db82cbe4e600dc0853e734c247607caea
Helton Mori <heltonmori@gmail.com>
2025-04-26 14:23:11 -0300

    Começando com testes

---
+++ tests/components/PageHeader.test.js
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import PageHeader from "../../src/components/PageHeader";
+import { I18nextProvider } from "react-i18next";
+import i18n from "../../src/i18n";
+
+describe("PageHeader component", () => {
+  test("Deve renderizar o título e botoes de idioma", () => {
+    render(
+      <I18nextProvider i18n={i18n}>
+        <PageHeader />
+      </I18nextProvider>
+    );
+    expect(screen.getByText("En")).toBeInTheDocument();
+    expect(screen.getByText("Pt")).toBeInTheDocument();
+    expect(screen.getByText("Helton Mori")).toBeInTheDocument();
+  });
+  test("Deve bater com o snapshot", () => {
+    const { container } = render(
+      <I18nextProvider i18n={i18n}>
+        <PageHeader />
+      </I18nextProvider>
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
